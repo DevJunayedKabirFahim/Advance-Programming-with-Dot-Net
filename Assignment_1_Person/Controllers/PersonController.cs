a19b5aff73b00a7a43b5eca79b04e81690ca0d4d
Junayed Kabir Fahim <junayedkabirfahim@gmail.com>
2022-05-25 02:35:05 +0600

    Assignment-1 Create Controller, Router, View

---
+++ Assignment_1_Person/Controllers/PersonController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_1_Person.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PersonalBackground()
+        {
+            return View();
+        }
+
+        public ActionResult EducationalBackground()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}
